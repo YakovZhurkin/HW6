7dfeb0a027b86c29a8d55083b88c6bae8ed8d54d
Yakov Zhurkin <zh4rk1n@yandex.ru>
2023-07-10 00:16:53 +0300

    HomeWork6

---
+++ task_41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[m];
+Chisla(m);
+Console.Write($"\n Чисел больше нуля: {kolicestvo0(Array)}");
+
+void Chisla(int m)
+{
+for (int i = 0; i < m; i++)
+{
+Console.WriteLine($"Введите {i+1} элемент массива ");
+Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+}
+
+int kolicestvo0(int[] Array)
+{
+int i=0;
+int kolvo = 0;
+while (i < Array.Length)
+{
+if(Array[i]>0)
+kolvo = kolvo + 1;
+i = i + 1;
+}
+return kolvo;
+}
+
---
+++ task_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double B1 = Prompt("Введите коориданту x первой прямой : ");
+double K1 = Prompt("Введите коориданту y первой прямой : ");
+double B2 = Prompt("Введите коориданту x второй прямой : ");
+double K2 = Prompt("Введите коориданту y второй прямой : ");
+tochka_per(B1,K1,B2,K2);
+double tochka_per(double b1,double k1,double b2,double k2) 
+{
+    double x = (-b2 + b1)/(-k1 + k2);
+    double y = k2 * x + b2;
+    Console.WriteLine($"Координаты точки пересечения прямы : {x}|{y}");
+    return 0;
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
